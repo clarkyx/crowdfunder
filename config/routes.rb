4408eb22b02d9282da3c0df8435f03d14b8e2e27
clark chen <chenhuajie1@gmail.com>
2016-09-21 17:50:55 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  root 'projects#index'
+
+  resources :sessions, only: [:index, :new, :create, :destroy]
+  resources :user, only: [:new, :create] do
+    resources :projects do
+      resources :rewards
+    end
+    resources :pledges do
+      resources :projects, only: [:show]
+    end
+  end
 end
